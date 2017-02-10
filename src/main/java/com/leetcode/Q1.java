7bd76e87a84a999b791227b83c788d166827787b
luckyp <yanpeng@live.com>
2017-02-10 13:28:45 +0800

    Q1

---
+++ src/main/java/com/leetcode/Q1.java
@@ -0,0 +1,29 @@
+package com.luckyp.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * Created by yanpeng on 2017/2/10.
+ */
+public class Q1 {
+    public static void main(String[] args) {
+
+        int[] nums = {3 ,2, 7, 11, 15};
+        int target = 13;
+        Q1 q1 = new Q1();
+        System.out.println(Arrays.toString(q1.twoSum(nums, target)));
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("no solution");
+    }
+}
